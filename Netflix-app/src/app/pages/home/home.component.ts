c87db9577a1c76c96cc34673167cda4fb5afe5a9
Mohammed Mishal <mishal2mk2@gmail.com>
2023-01-09 19:33:17 +0530

    home page added trending

--- Netflix-app/src/app/pages/home/home.component.ts
+++ Netflix-app/src/app/pages/home/home.component.ts
@@ -4,22 +4,30 @@ import { MovieApiServiceService } from 'src/app/service/movie-api-service.servic
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
 
-  constructor(private service:MovieApiServiceService) { }
-
-  bannerResult:any=[];
+  bannerResult: any = [];
+  trendingResult: any = [];
 
   ngOnInit(): void {
-    this.bannerData()
+    this.bannerData();
+    this.trendingData();
   }
 
-  bannerData(){
-    this.service.bannerApiData().subscribe((result)=>{
-      console.log(result,'bannerresult#')
-      this.bannerResult=result.results;
-    })
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      // console.log(result,'bannerresult#')
+      this.bannerResult = result.results;
+    });
+  }
+
+  trendingData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      console.log(result, 'trendingresult#');
+      this.trendingResult=result.results;
+    });
   }
 }
--- Netflix-app/src/app/service/movie-api-service.service.ts
+++ Netflix-app/src/app/service/movie-api-service.service.ts
@@ -11,8 +11,13 @@ export class MovieApiServiceService {
   baseUrl="http://api.themoviedb.org/3";
   apikey = "08cc33bd5ae3a747598ce2ad84376e66";
   bannerapi=`${this.baseUrl}/trending/all/week?api_key=${this.apikey}`;
+  trendingapi=`${this.baseUrl}/trending/movie/day?api_key=${this.apikey}`
 
   bannerApiData():Observable<any>{
     return this.http.get(this.bannerapi)
+  }
+
+  trendingMovieApiData():Observable<any>{
+    return this.http.get(this.trendingapi);
   }
 }
