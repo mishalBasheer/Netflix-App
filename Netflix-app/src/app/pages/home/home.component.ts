a6c216e5e00883b6e29e6bf0e088c2e65e1f0375
Mohammed Mishal <mishal2mk2@gmail.com>
2023-01-10 16:09:08 +0530

    categories of movies in home page

--- Netflix-app/src/app/pages/home/home.component.ts
+++ Netflix-app/src/app/pages/home/home.component.ts
@@ -11,23 +11,86 @@ export class HomeComponent implements OnInit {
 
   bannerResult: any = [];
   trendingResult: any = [];
+  actionMovieResult: any = [];
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+  documentaryMovieResult: any = [];
+  sciencefictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
 
   ngOnInit(): void {
     this.bannerData();
     this.trendingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.comedyMovie();
+    this.animationMovie();
+    this.documentaryMovie();
+    this.sciencefictionMovie();
+    this.thrillerMovie();
   }
 
+  // banner
   bannerData() {
     this.service.bannerApiData().subscribe((result) => {
-      // console.log(result,'bannerresult#')
       this.bannerResult = result.results;
     });
   }
 
+  // trending
   trendingData() {
     this.service.trendingMovieApiData().subscribe((result) => {
-      console.log(result, 'trendingresult#');
-      this.trendingResult=result.results;
+      this.trendingResult = result.results;
+    });
+  }
+
+  // action
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieResult = result.results;
+    });
+  }
+
+  // adventure
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieResult = result.results;
+    });
+  }
+
+  // animation
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieResult = result.results;
+    });
+  }
+
+  // comedy
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieResult = result.results;
+    });
+  }
+
+  // documentary
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      this.documentaryMovieResult = result.results;
+    });
+  }
+
+  // science-fiction
+  sciencefictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      this.sciencefictionMovieResult = result.results;
+    });
+  }
+
+  // thriller
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResult = result.results;
     });
   }
 }
